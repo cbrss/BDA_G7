c6bb7fec9f2b2d1ee3c0d5c9a2f3e8b4a1d2b2eb
Lautaro307 <97195752+Lautaro307@users.noreply.github.com>
2024-06-12 19:27:09 -0300

    ward por las dudas

--- entrega/03_CreacionTablas.sql
+++ entrega/03_CreacionTablas.sql
@@ -5,7 +5,7 @@
 		INTEGRANTES:
 			Cristian Raul Berrios Lima		42875289
 			Lautaro Da silva				42816815
-			Abigail Karina Peñafiel Huayta	41913506
+			Abigail Karina PeÃ±afiel Huayta	41913506
 
 		FECHA DE ENTREGA: 14/6/2024
 */
@@ -44,6 +44,8 @@ BEGIN
 		usr_actualizacion	DATE,
 		borrado_logico		BIT DEFAULT 0,
 
+		CONSTRAINT CK_Pacientid CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT CK_PacientNumdoc CHECK(ISNUMERIC(num_doc) = 1),
 		CONSTRAINT PK_PacienteID PRIMARY KEY (id)
 	);
 END;
@@ -70,6 +72,8 @@ BEGIN
 		img_resultado		VARCHAR(max),
 		borrado_logico		BIT DEFAULT 0,
 
+		CONSTRAINT CK_Estudioid CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT CK_Estudioid_paciente CHECK(ISNUMERIC(id_paciente) = 1),
 		CONSTRAINT PK_EstudioID PRIMARY KEY(id),
 		CONSTRAINT FK_Estudio_PacienteID FOREIGN KEY(id_paciente) REFERENCES gestion_paciente.Paciente(id)
 	);
@@ -93,6 +97,8 @@ BEGIN
 		contrasena			VARCHAR(30),
 		fecha_creacion		DATE,
 
+		CONSTRAINT CK_UsuarioId CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT CK_UsuarioId_paciente CHECK(ISNUMERIC(id_paciente) = 1),
 		CONSTRAINT PK_UsuarioID PRIMARY KEY(id),
 		CONSTRAINT FK_Usuario_PacienteID FOREIGN KEY(id_paciente) REFERENCES gestion_paciente.Paciente(id)
 	);
@@ -110,17 +116,23 @@ IF NOT EXISTS (
 BEGIN
     CREATE TABLE gestion_paciente.Domicilio
 	(
-		id					INT IDENTITY(1,1),
+		id				INT IDENTITY(1,1),
 		id_paciente			INT,
 		calle				VARCHAR(30),
 		numero				INT,
 		piso				INT,
-		departamento		INT,
+		departamento			INT,
 		cod_postal			INT,
 		pais				VARCHAR(30),
 		provincia			VARCHAR(30),
 		localidad			VARCHAR(30),
 
+		CONSTRAINT CK_DomicId CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT CK_DomicIdpaciente CHECK(ISNUMERIC(id_paciente) = 1),
+		CONSTRAINT CK_Domicnumero CHECK(ISNUMERIC(numero) = 1),
+		CONSTRAINT CK_Domicpiso CHECK(ISNUMERIC(piso) = 1),
+		CONSTRAINT CK_Domicdepart CHECK(ISNUMERIC(departamento) = 1),
+		CONSTRAINT CK_DomicCodpostal CHECK(ISNUMERIC(cod_postal) = 1),
 		CONSTRAINT PK_DomicilioID PRIMARY KEY (id),
 		CONSTRAINT FK_Domicilio_PacienteID FOREIGN KEY (id_paciente) REFERENCES gestion_paciente.Paciente(id)
 	);
@@ -139,12 +151,15 @@ IF NOT EXISTS (
 BEGIN
     CREATE TABLE gestion_paciente.Cobertura
 	(
-		id					INT,
+		id				INT,
 		id_paciente			INT,
 		imagen_credencial	VARCHAR(max),
 		nro_socio			INT,
 		fecha_registro		DATE,
 
+		CONSTRAINT CK_Cobertid CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT CK_Cobertid_paciente CHECK(ISNUMERIC(id_paciente) = 1),
+		CONSTRAINT CK_Cobertnro_socio CHECK(ISNUMERIC(nro_socio) = 1),
 		CONSTRAINT PK_CoberturaID PRIMARY KEY (id),
 		CONSTRAINT FK_Cobertura_PacienteID FOREIGN KEY (id_paciente) REFERENCES gestion_paciente.Paciente(id)
 	);
@@ -168,6 +183,8 @@ BEGIN
 		nombre				VARCHAR(30),
 		[plan]				VARCHAR(30),
 
+		CONSTRAINT CK_Prestaid CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT CK_Prestaidcobertura CHECK(ISNUMERIC(id_cobertura) = 1),
 		CONSTRAINT PK_PrestadorID PRIMARY KEY (id),
 		CONSTRAINT FK_Prestador_CoberturaID FOREIGN KEY (id_cobertura) REFERENCES gestion_paciente.Cobertura(id)
 	);
@@ -188,13 +205,14 @@ IF NOT EXISTS (
 )
 BEGIN
 	 CREATE TABLE gestion_sede.Sede (
-		 id				INT IDENTITY(1,1),
+		 id			INT IDENTITY(1,1),
 		 nombre			VARCHAR(30),
 		 direccion		VARCHAR(30),
 		 localidad		VARCHAR(30),
 		 provincia		VARCHAR(30),
 
-		 CONSTRAINT PK_SedeID PRIMARY KEY (id)
+		CONSTRAINT CK_Sedeid CHECK(ISNUMERIC(id) = 1), 
+		CONSTRAINT PK_SedeID PRIMARY KEY (id)
 	 )
 END
 GO
@@ -209,11 +227,14 @@ IF NOT EXISTS (
 )
 BEGIN
 	 CREATE TABLE gestion_sede.Medico(
-		 id				INT IDENTITY(1,1),
+		 id			INT IDENTITY(1,1),
 		 nombre			VARCHAR(25),
 		 apellido		VARCHAR(20),
 		 matricula		INT UNIQUE,
-		 CONSTRAINT PK_MedicoID PRIMARY KEY (id)
+
+		CONSTRAINT CK_Medicid CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT CK_Medicmatricula CHECK(ISNUMERIC(matricula) = 1),
+		CONSTRAINT PK_MedicoID PRIMARY KEY (id)
 	 );
 END;
 GO
@@ -234,6 +255,9 @@ BEGIN
 		 dia			DATE,
 		 hora_inicio	TIME,
 
+	 CONSTRAINT CK_Diasid CHECK(ISNUMERIC(id) = 1),
+	 CONSTRAINT CK_Diasidsede CHECK(ISNUMERIC(id_sede) = 1),
+	 CONSTRAINT CK_Diasidmedico CHECK(ISNUMERIC(id_medico) = 1),
 	 CONSTRAINT PK_DiasxsedeID	PRIMARY KEY (id),
 	 CONSTRAINT FK_SedeID		FOREIGN KEY (id_sede)	REFERENCES gestion_sede.Sede(id),
 	 CONSTRAINT FK_MedicoID		FOREIGN KEY (id_medico) REFERENCES gestion_sede.Medico(id)
@@ -254,6 +278,8 @@ BEGIN
 		id			INT,
 		nombre		VARCHAR(20),
 
+		CONSTRAINT CK_Espid CHECK(ISNUMERIC(id) = 1),
+		CONSTRAINT Ck_Espnomb CHECK(PATINDEX('%[^A-Za-z]%', nombre) = 0),
 		CONSTRAINT PK_EspecialidadID PRIMARY KEY (id)
 	 );
 END;
